3bf44f47c122934661fc5530ff2633c6697f0fa7
Black-1sh <shmatkovevgeniy@gmail.com>
2025-05-06 09:56:40 +0800

    u
    u

--- shmatkov_compgraph/shmatkov_compgraph/shmatkov_compgraph.cpp
+++ shmatkov_compgraph/shmatkov_compgraph/shmatkov_compgraph.cpp
@@ -9,27 +9,21 @@
 #include "glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
-
-// Трапеция (4 точки)
 GLfloat points[] = {
-    -0.5f,  0.5f,  0.0f,  // Верхний левый
-     0.5f,  0.5f,  0.0f,  // Верхний правый
-    -0.8f, -0.5f,  0.0f,  // Нижний левый
-     0.8f, -0.5f,  0.0f   // Нижний правый
+    -0.5f,  0.5f,  0.0f,  
+     0.5f,  0.5f,  0.0f, 
+    -0.8f, -0.5f,  0.0f, 
+     0.8f, -0.5f,  0.0f   
 };
-
 GLint index[] = {
-    0, 1, 2,  // Первый треугольник
-    1, 2, 3   // Второй треугольник
+    0, 1, 2, 
+    1, 2, 3   
 };
-
 const unsigned int SCR_WIDTH = 1024;
 const unsigned int SCR_HEIGHT = 768;
-
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-
 bool firstMouse = true;
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
@@ -38,7 +32,6 @@ float pitch = 0.0f;
 float fov = 45.0f;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
-
 void processInput(GLFWwindow* window)
 {
     float cameraSpeed = 2.5f * deltaTime;
@@ -53,7 +46,6 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
-
 void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 {
     float xpos = static_cast<float>(xposIn);
@@ -68,26 +60,21 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
     float yoffset = lastY - ypos;
     lastX = xpos;
     lastY = ypos;
-
     float sensitivity = 0.1f;
     xoffset *= sensitivity;
     yoffset *= sensitivity;
-
     yaw += xoffset;
     pitch += yoffset;
-
     if (pitch > 89.0f)
         pitch = 89.0f;
     if (pitch < -89.0f)
         pitch = -89.0f;
-
     glm::vec3 front;
     front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
     front.y = sin(glm::radians(pitch));
     front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
     cameraFront = glm::normalize(front);
 }
-
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     fov -= (float)yoffset;
@@ -96,7 +83,6 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
     if (fov > 45.0f)
         fov = 45.0f;
 }
-
 int main()
 {
     if (!glfwInit()) {
@@ -114,7 +100,6 @@ int main()
         glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(window);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
@@ -126,12 +111,10 @@ int main()
         fprintf(stderr, "Error: %s\n", glewGetErrorString(ret));
         return 1;
     }
-
     GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
-
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
@@ -141,44 +124,31 @@ int main()
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-
     ShaderLoader shader("vert_shader.glsl", "frag_shader.glsl");
     GLuint shaderProgram = shader.getProgram();
-
     while (!glfwWindowShouldClose(window)) {
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
-
         processInput(window);
-
-        // Установка цвета фона (1.0, 1.0, 0.3)
         glClearColor(1.0f, 1.0f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
         glUseProgram(shaderProgram);
-
         glm::mat4 projection = glm::perspective(glm::radians(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         glm::mat4 model = glm::mat4(1.0f);
-
         unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
         unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-        // Установка цвета трапеции (0.5, 0.5, 1.0)
         shader.setUniform("ourColor", 0.5f, 0.5f, 1.0f, 1.0f);
-
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
